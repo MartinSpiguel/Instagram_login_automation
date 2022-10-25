1a1b12e1beae2e6a71ba4e6bacf551f52cfc3607
Martin Spiguel <maspiguel@gmail.com>
2022-10-25 20:28:30 -0300

    Input fields

---
+++ instagram_login_automation.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+my_username = input('Username: ')
+my_password = input('Password: ')
+
+driver = webdriver.Edge() 
+driver.get('https://www.instagram.com')
+assert 'Instagram' in driver.title
+
+driver.maximize_window()
+driver.implicitly_wait(5)
+
+username = driver.find_element(By.NAME, 'username')
+password = driver.find_element(By.NAME, 'password')
+
+username.clear()
+password.clear()
+
+username.send_keys(my_username)
+password.send_keys(my_password)
+
+username.send_keys(Keys.RETURN)
+password.send_keys(Keys.RETURN)
+
+time.sleep(15)
+
+driver.close()
